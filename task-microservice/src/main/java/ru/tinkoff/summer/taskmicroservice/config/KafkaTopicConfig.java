2d0604d32e6ee2dbb81d972ae199fe26f869d656
Nikita Vashkulatov <vashkulatov.nikita@promregion.net>
2023-11-15 22:42:19 +0300

    fix kafka admin link

--- task-microservice/src/main/java/ru/tinkoff/summer/taskmicroservice/config/KafkaTopicConfig.java
+++ task-microservice/src/main/java/ru/tinkoff/summer/taskmicroservice/config/KafkaTopicConfig.java
@@ -1,5 +1,6 @@
 package ru.tinkoff.summer.taskmicroservice.config;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,24 +8,38 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
 
 
-
+import java.util.HashMap;
+import java.util.Map;
 
 import static ru.tinkoff.summer.taskshareddomain.ConnectionConstants.*;
 
 @Configuration
 public class KafkaTopicConfig {
 
-    Logger logger = LoggerFactory.getLogger(KafkaTopicConfig.class);
+    @Value(value = "${spring.kafka.consumer.bootstrap-servers}")
+    private String bootstrapAddress;
+
     @Bean
-    public NewTopic attemptTopic(){
-        logger.error("Atttempts topics = {}" ,ATTEMPT_TOPIC_PARTITIONS);
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+
+    Logger logger = LoggerFactory.getLogger(KafkaTopicConfig.class);
+
+    @Bean
+    public NewTopic attemptTopic() {
+        logger.error("Atttempts topics = {}", ATTEMPT_TOPIC_PARTITIONS);
         return TopicBuilder.name(ATTEMPT_TOPIC_NAME).partitions(ATTEMPT_TOPIC_PARTITIONS).build();
     }
+
     @Bean
-    public NewTopic resultTopic(){
-        logger.error("Result topics = {}" ,RESULT_TOPIC_PARTITIONS);
+    public NewTopic resultTopic() {
+        logger.error("Result topics = {}", RESULT_TOPIC_PARTITIONS);
         return TopicBuilder.name(RESULT_TOPIC_NAME).partitions(RESULT_TOPIC_PARTITIONS).build();
     }
 }
